87189603761714706c79c1ea55943432500d8926
Angelok27 <gelya.voronova.0202@mail.ru>
2020-04-25 20:32:35 +0700

    Changed: geom.h

--- geom.h
+++ geom.h
@@ -3,13 +3,15 @@
 #define Pi 3.14
 
 typedef struct { // определение структуры
-    float R;
-    float x;
-    float y;
+    float r[256];
+    float x[256];
+    float y[256];
 
 } Circle;
 
 void Output_circle(Circle* qwerty_1, int j);
 void Input_circle(Circle* qwerty, int j);
-
+int area(Circle* c, int n);
+int per(Circle* c, int n);
+void intersects(Circle* c, int j, int n);
 #endif
