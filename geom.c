883d99fb8adcfde995a5db6d5046679ad8437e27
Angelok27 <gelya.voronova.0202@mail.ru>
2020-05-21 22:21:52 +0700

    add geom.c

--- geom.c
+++ geom.c
@@ -4,18 +4,17 @@
 //#define Pi 3.14
 #include "geom.h"
 
-
 int area(Circle* c, int j)
 {
-    float area;
-      area = c->r[j] * c->r[j] * Pi;
-      return area;
+    float area
+    area = c->r[j] * c->r[j] * Pi;
+    return area;
 
 }
 
 int per(Circle* c, int j)
 {
     float perimetr;
-      perimetr = c->r[j] * 2 * Pi;
+    perimetr = c->r[j] * 2 * Pi;
     return perimetr;
 }
